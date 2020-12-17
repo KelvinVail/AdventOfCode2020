944f35b45c2880550b4eea4b37074c4137413f43
KelvinVail <26182999+KelvinVail@users.noreply.github.com>
2020-12-17 20:37:54 +0000

    Day 1, Part 1 complete

---
+++ src/Main/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using Day01;
+
+namespace Main
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"..\..\..\..\Day01\PuzzleInput.txt");
+            var expenses = lines.Select(line => int.Parse(line, NumberStyles.Integer, CultureInfo.CurrentCulture)).ToList();
+
+            var expenseReport = new ExpenseReport();
+            expenseReport.SetExpenses(expenses);
+
+            Console.WriteLine(expenseReport.Fix());
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
