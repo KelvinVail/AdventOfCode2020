57cd7e4563dde4e4749c52246335b0ed0246b3af
KelvinVail <26182999+KelvinVail@users.noreply.github.com>
2020-12-17 19:57:25 +0000

    Day 1, initial solution

---
+++ src/Day01.Tests/Day01Tests.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace Day01.Tests
+{
+    public class Day01Tests
+    {
+        private readonly ExpenseReport _expenseReport = new ExpenseReport();
+
+        [Fact]
+        public void FixReturnsZero()
+        {
+            Assert.Equal(0, _expenseReport.Fix());
+        }
+
+        [Theory]
+        [InlineData(1010, 1010)]
+        [InlineData(1009, 1011)]
+        public void FixReturnsProductOfNumbersThatAddUpTo2020(int one, int two)
+        {
+            var expenses = new List<int> { one, two };
+            _expenseReport.SetExpenses(expenses);
+
+            var result = _expenseReport.Fix();
+
+            Assert.Equal(one * two, result);
+        }
+
+        [Theory]
+        [InlineData(1, 1)]
+        public void FixReturnsZeroIfNumbersDoNotAddUpTo2020(int one, int two)
+        {
+            var expenses = new List<int> { one, two };
+            _expenseReport.SetExpenses(expenses);
+
+            var result = _expenseReport.Fix();
+
+            Assert.Equal(0, result);
+        }
+
+        [Theory]
+        [InlineData(1010, 1010, 10, 1010 * 1010)]
+        [InlineData(1009, 88, 1011, 1009 * 1011)]
+        [InlineData(1010, 1009, 1011, 1009 * 1011)]
+        public void FixReturnsProductOfAnyTwoNumbersThatAddUpTo2020(
+            int one,
+            int two,
+            int three,
+            int expected)
+        {
+            var expenses = new List<int> { one, two, three };
+            _expenseReport.SetExpenses(expenses);
+
+            var result = _expenseReport.Fix();
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ src/Day01/ExpenseReport.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Day01
+{
+    public class ExpenseReport
+    {
+        private IEnumerable<int> _expenses;
+
+        public void SetExpenses(IEnumerable<int> expenses)
+        {
+            _expenses = expenses;
+        }
+
+        public int Fix()
+        {
+            if (_expenses is null) return 0;
+            var itemCount = _expenses.Count();
+            for (int i = 0; i < itemCount; i++)
+            {
+                var baseExpense = _expenses.ElementAt(i);
+                for (int j = 0; j < itemCount; j++)
+                {
+                    if (i == j) continue;
+                    if (baseExpense + _expenses.ElementAt(j) == 2020)
+                        return baseExpense * _expenses.ElementAt(j);
+                }
+            }
+
+            return 0;
+        }
+    }
+}
